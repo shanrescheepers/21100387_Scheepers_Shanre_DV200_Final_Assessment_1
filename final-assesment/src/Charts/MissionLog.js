8cf437bd33fdc152f575ea005a70248aba711b0e
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-03-30 18:07:16 +0200

    .

--- final-assesment/src/Charts/MissionLog.js
+++ final-assesment/src/Charts/MissionLog.js
@@ -3,21 +3,21 @@ import React, { useState, useEffect } from "react";
 import { getThrust, getLaunches, missionLog } from "../services/ApiService";
 
 export function missionLog() {
-    data.forEach(function (data) {
-        {
-            $(".missions").append(`
-                `
-                `<Card style={{ width: '18rem' }}>
-        <Card.Body>
-            < Card.Title > + { mission_name[i].data } +</ >
-           ` < Card.Subtitle className = "mb-2 text-muted" > `+ { mission_name[i].data } +</>
-            `< Card.Text >
-            `  Some quick example text to build on the card title and make up the bulk of
-               ` the card's content.
-                `</>
-            < Card.Link href = "#" > `+ { mission_name[i].data } +</>
-            < Card.Link href="#" > Another Link</ >\
-                `</Card.Body >
-    </Card > `
+    //     data.forEach(function (data) {
+    //         {
+    //             $(".missions").append(`
+    //                 `
+    //                 `<Card style={{ width: '18rem' }}>
+    //         <Card.Body>
+    //             < Card.Title > + { mission_name[i].data } +</ >
+    //            ` < Card.Subtitle className = "mb-2 text-muted" > `+ { mission_name[i].data } +</>
+    //             `< Card.Text >
+    //             `  Some quick example text to build on the card title and make up the bulk of
+    //                ` the card's content.
+    //                 `</>
+    //             < Card.Link href = "#" > `+ { mission_name[i].data } +</>
+    //             < Card.Link href="#" > Another Link</ >\
+    //                 `</Card.Body >
+    //     </Card > `
 }
-        export default missionLog;
+export default missionLog;
