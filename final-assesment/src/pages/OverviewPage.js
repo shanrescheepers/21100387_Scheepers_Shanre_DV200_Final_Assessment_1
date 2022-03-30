0df8e3919cbcc3f7710d1f7ca5fbdb24b7832abb
Shanre Scheepers <21100387@virtualwindow.co.za>
2022-03-30 14:44:06 +0200

    ADD: ChartOne + OverviewPage

---
+++ final-assesment/src/pages/OverviewPage.js
@@ -0,0 +1,10 @@
+import { getLaunches } from "../services/ApiService"
+
+export function OverviewPage() {
+    return (
+        <section>
+            <h1>Space X: Overview</h1>
+
+        </section>
+    )
+}
